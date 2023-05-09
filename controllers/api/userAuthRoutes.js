86ce91bc230292683dd3311a2b527e1f37e9dd32
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-05-09 13:11:38 -0500

    i already finished the userAuthRoutes.js file, since most of this code is very similar to what I used for my MVC tech blog

--- controllers/api/userAuthRoutes.js
+++ controllers/api/userAuthRoutes.js
@@ -24,3 +24,58 @@ router.post("/signup", async (req, res) => {
     res.status(400).json(err);
   }
 });
+
+// Login a user
+router.post("/login", async (req, res) => {
+  try {
+    console.log("Request body:", req.body);
+
+    const loginUser = await User.findOne({
+      where: { email: req.body.email },
+    });
+
+    console.log("Login user:", loginUser);
+
+    if (!loginUser) {
+      res.status(400).json({
+        message: "Incorrect email or/and password. Please enter again.",
+      });
+      return;
+    }
+
+    const validPassword = await loginUser.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({
+        message: "Incorrect email or/and password. Please enter again.",
+      });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = loginUser.id;
+      req.session.username = loginUser.username;
+      req.session.loggedIn = true;
+
+      res
+        .status(200)
+        .json({ user: loginUser, message: "Yahhh! You are logged in!" });
+    });
+  } catch (err) {
+    console.error("Error in login route:", err);
+    res.status(500).json(err);
+  }
+});
+
+// Logout a user
+router.post("/logout", authRequired, (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
