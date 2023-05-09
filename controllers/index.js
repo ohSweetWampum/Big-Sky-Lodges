f7046b5f9e6add574fc0f877042a03fca745ee93
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-05-09 12:48:01 -0500

    completed both index.js files in the controllers folder

---
+++ controllers/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+const apiRoutes = require("./api");
+const homeRoutes = require("./homeRoutes");
+
+router.use("/api", apiRoutes);
+router.use(homeRoutes);
+
+module.exports = router;
