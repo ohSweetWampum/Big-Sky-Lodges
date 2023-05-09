3d7da906415775ed5fb0e634d8697109b76fb23f
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-05-09 14:09:20 -0500

    started with a template for each seed file. will finish and update it later

---
+++ seeds/seedReservationData.js
@@ -0,0 +1,17 @@
+const reservationData = [
+  {
+    user_id: 1,
+    room_id: 1,
+    check_in_date: "2023-06-01",
+    check_out_date: "2023-06-07",
+  },
+  {
+    user_id: 2,
+    room_id: 2,
+    check_in_date: "2023-06-10",
+    check_out_date: "2023-06-14",
+  },
+  // Add more reservations
+];
+
+module.exports = reservationData;
---
+++ seeds/seedUserData.js
@@ -0,0 +1,15 @@
+const userData = [
+  {
+    username: "user1",
+    email: "user1@example.com",
+    password: "password1",
+  },
+  {
+    username: "user2",
+    email: "user2@example.com",
+    password: "password2",
+  },
+  // Add more users
+];
+
+module.exports = userData;
