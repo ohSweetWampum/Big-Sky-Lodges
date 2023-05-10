da98f65d9bb39c058d720a5a334176159afe43df
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-05-09 20:28:40 -0500

    fixed some typos to make the db seed and the server start

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,5 @@
-const Sequelize = require("sequelize");
 require("dotenv").config();
-
+const Sequelize = require("sequelize");
 let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
