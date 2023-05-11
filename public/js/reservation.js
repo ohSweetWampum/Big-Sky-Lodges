e7d7fd9e06bd2114dd124401790aa54f712f8fd5
unknown <eogbeide2@gmail.com>
2023-05-11 01:40:36 -0700

    made some changes to the client side code

--- public/js/reservation.js
+++ public/js/reservation.js
@@ -1,18 +1,18 @@
 const check_in = document.querySelector('#check-in').value;
 const check_out = document.querySelector('#check-out').value;
 const branch = document.querySelector('#branch').value;
-const roomType = document.querySelecto('#room-type').value;
-const numGuest = document.querySelector('#num-guest').value;
+const room_id = document.querySelecto('#room-type').value;
+const num_guests = document.querySelector('#num-guest').value;
 const check_available= document.querySelector('#check-avail').value;
 
 
 async function reservationSubmission(event) {
     event.preventDefault();
 
-    if(check_in && check_out && branch && roomType && numGuest ) {
+    if(check_in && check_out && branch && room_id && num_guests ) {
         const response = fetch('api/booknow',{
         method: 'Post',
-        body: JSON.stringify({check_in, check_out,branch,roomType,numGuest}),
+        body: JSON.stringify({check_in, check_out,room_id,num_guests}),
         headers: {
             'Content-Type': 'application/json',
         },
