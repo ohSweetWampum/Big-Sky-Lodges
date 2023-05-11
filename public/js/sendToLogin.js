cf4562dbc46de4f81a5094812d7bb20ecae74b5b
unknown <eogbeide2@gmail.com>
2023-05-10 18:37:14 -0700

    fixed css

--- public/js/sendToLogin.js
+++ public/js/sendToLogin.js
@@ -1,25 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const dashboardLinks = document.querySelectorAll(".dashboard-link");
+// document.addEventListener("DOMContentLoaded", () => {
+//   const dashboardLinks = document.querySelectorAll(".dashboard-link");
 
-  dashboardLinks.forEach((link) => {
-    link.addEventListener("click", async (event) => {
-      event.preventDefault();
-      try {
-        const response = await fetch("/api/users/checkLoggedIn", {
-          headers: { "Content-Type": "application/json" },
-        });
+//   dashboardLinks.forEach((link) => {
+//     link.addEventListener("click", async (event) => {
+//       event.preventDefault();
+//       try {
+//         const response = await fetch("/api/users/checkLoggedIn", {
+//           headers: { "Content-Type": "application/json" },
+//         });
 
-        if (response.ok) {
-          const userId = await response.text();
+//         if (response.ok) {
+//           const userId = await response.text();
 
-          document.location.replace(`/users/${userId}/reservations`);
-        } else {
-          document.location.replace("/login");
-        }
-      } catch (error) {
-        console.error("Error:", error);
-        document.location.replace("/login");
-      }
-    });
-  });
-});
+//           document.location.replace(`/users/${userId}/reservations`);
+//         } else {
+//           document.location.replace("/login");
+//         }
+//       } catch (error) {
+//         console.error("Error:", error);
+//         document.location.replace("/login");
+//       }
+//     });
+//   });
+// });
